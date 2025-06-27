917a90bf4d9813e50519ec8d6869706026d7a602
whynotdeep7 <deepakchoudhary3365@gmail.com>
2025-06-27 11:08:13 +0530

    update index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:"/shudd/",
+  base: "/shudd/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './src/main.tsx'
-    }
-  }
 })
